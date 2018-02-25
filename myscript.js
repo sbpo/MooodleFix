4880c6b1ce8a692373f21ea5bb84b0d0a5694b29
sbpo <sboelpoulsen@gmail.com>
2018-02-25 14:27:54 +0100

    første commit

---
+++ myscript.js
@@ -0,0 +1,9 @@
+/*docReady(() => {
+  let elements = document.getElementsByClassName("coursebox");
+  console.log(elements);
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].className="coursebox2";
+    elements[i].setAttribute('class', 'coursebox2')
+    console.log(elements[i].className);
+  }
+});*/
